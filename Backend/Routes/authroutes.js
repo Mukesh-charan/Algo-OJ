de759081e1fe968190db1722c7cc8938523fbeeb
mukeshcharan2905@gmail.com <mukeshcharan2905@gmail.com>
2025-08-28 22:46:26 +0530

    Random compile Final

--- Backend/Routes/authroutes.js
+++ Backend/Routes/authroutes.js
@@ -1,17 +1,19 @@
 import express from 'express';
 import { loginUser, createUser, getUser, deleteUser, updateUser, getUsers, updateUserRole } from '../Controllers/authControllers.mjs';
-import { authenticateJWT } from '../Middleware/authMiddleware.js';
+
 
 const router = express.Router();
 
 router.post('/register', createUser);
+
+router.put('/:id', updateUser);
+
 router.post('/login', loginUser);
 
-router.get('/users', authenticateJWT, getUsers);
-router.get('/users/:id', authenticateJWT, getUser);
-router.delete('/users/:id', authenticateJWT, deleteUser);
-router.put('/:id', authenticateJWT, updateUser);
-router.put('/users/:id', authenticateJWT, updateUserRole);
+router.get('/users', getUsers);
+router.get('/users/:id', getUser);
+router.delete('/users/:id', deleteUser);
+router.put('/users/:id', updateUserRole);
 
 export default router;
 
