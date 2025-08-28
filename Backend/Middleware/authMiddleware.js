9478406fbe9b1edbcb639a85dffb74a133e7e4cb
mukeshcharan2905@gmail.com <mukeshcharan2905@gmail.com>
2025-08-28 22:42:40 +0530

    Random compile Final

--- Backend/Middleware/authMiddleware.js
+++ Backend/Middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../Models/Users.js';
 
 export const authenticateJWT = async (req, res, next) => {
   const token = req.headers.authorization && req.headers.authorization.split(" ")[1];
